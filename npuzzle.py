081d8eedd8efbe1c497c4ff1597af431f1df4611
vihaanmathur <161659378+vihaanmathur@users.noreply.github.com>
2024-02-29 13:28:51 -0800

    Update and rename Mathur_V_U1_L6.py to npuzzle.py

--- npuzzle.py
+++ npuzzle.py
@@ -1,5 +1,9 @@
 # Name: Vihaan Mathur  
 # Date: 9/29/21
+# Uses a Heap Priority Queue Data Structure to implement the n-puzzle problem. 
+#Also uses the A* and bi-directional BFS (Breadth-First Search) algorithms. 
+#The 15-puzzle is a sliding puzzle that consists of a frame of numbered square tiles in random order with one tile missing. 
+#The goal is to arrange the tiles into a specific order by making sliding moves that use the empty space. 
 import random, time, math
 
 class HeapPriorityQueue():
